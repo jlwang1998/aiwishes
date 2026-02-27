97d78d19dc4f76f3238a1b558b4be6c1e5a0655b
phil <jlwang1998@163.com>
2026-02-27 17:30:00 +0800

    修复数据库不同步的问题

--- AiWishes/models/user.py
+++ AiWishes/models/user.py
@@ -4,6 +4,8 @@ from sqlalchemy import Integer, String, DateTime
 from pwdlib import PasswordHash
 from datetime import datetime
 
+password_hash = PasswordHash.recommended()
+
 class User(Base):
     __tablename__ = "users"
 
@@ -11,29 +13,27 @@ class User(Base):
     email: Mapped[str] = mapped_column(String(100), unique=True)
     username: Mapped[str] = mapped_column(String(100))
     # 私有字段，存储哈希后的密码，字符串类型最大长度200
-    _password: Mapped[str] = mapped_column(String(100))
+    _password: Mapped[str] = mapped_column(String(200))
 
     """
     *args：可以接收任何数量的普通信息
     **kwargs：可以接收任何数量的带名字的信息"""
     def __init__(self, *args, **kwargs):
-        password = kwargs.get("password")
-        if password:
-            password = kwargs.pop("password")
-        #使用父类方法初始化参数
+        password = kwargs.pop('password')
         super().__init__(*args, **kwargs)
-        self.password = password#触发setter方法
-        #@property是Python的魔法装饰器，它会把方法变成属性
+        if password:
+            self.password = password
+    #@property是Python的魔法装饰器，它会把方法变成属性
     @property
     def password(self):
         return self._password
     
     @password.setter
-    def password(self, password):
-        self._password = PasswordHash().hash(password)
+    def password(self, raw_password):
+        self._password = password_hash.hash(raw_password)
 
-    def check_password(self, password):
-        return PasswordHash().verify(password, self._password)
+    def check_password(self, raw_password):
+        return password_hash.verify(raw_password, self.password)
         
 class EmailCode(Base):
     __tablename__ = "email_code"
@@ -42,6 +42,3 @@ class EmailCode(Base):
     email: Mapped[str] = mapped_column(String(100))
     code: Mapped[str] = mapped_column(String(10))
     created_time: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
-
-    
-    
